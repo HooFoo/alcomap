dd626f70bd255598273d264d20df9186f708a778
HooFoo <lHooFool@gmail.com>
2015-10-06 21:50:14 +0300

    Spec deletion

--- spec/models/chat_message_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe ChatMessage do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/models/rated_point_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe RatedPoint do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
