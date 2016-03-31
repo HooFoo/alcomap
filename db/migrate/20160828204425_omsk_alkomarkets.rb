e0c7400e47884d576587fae832aa97822ed192bc
Gennadiy Khudyakov <lHooFool@gmail.com>
2016-04-01 02:50:37 +0300

    Points

--- db/migrate/20160828204425_omsk_alkomarkets.rb
+++ db/migrate/20160828204425_omsk_alkomarkets.rb
@@ -1,28 +1,28 @@
 class OmskAlkomarkets < ActiveRecord::Migration
   def up
-    Point.new(:user => User.find_by_id(1),:lng =>"73.315657",  :lat => "54.985305", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.255034",  :lat => "55.031359", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.312942",  :lat => "54.982326", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.303356",  :lat => "54.990044", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.411408",  :lat => "54.9728", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.354133",  :lat => "54.998634", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.313172",  :lat => "54.942345", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.416614",  :lat => "54.990864", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.400404",  :lat => "54.990624", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.295948",  :lat => "54.999107", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.305626",  :lat => "55.046514", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.270701",  :lat => "55.035708", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.378323",  :lat => "55.030199", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.262892",  :lat => "55.041116", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.389697",  :lat => "54.94084", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.440196",  :lat => "54.942831", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.444189",  :lat => "54.961663", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.218931",  :lat => "55.03332", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.455394",  :lat => "54.915539", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.465871",  :lat => "54.92583", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
-    Point.new(:user => User.find_by_id(1),:lng =>"73.508336",  :lat => "54.934125", :point_type => "shop", :name => "Алкоголь 24 часа, сеть продовольственных магазинов", :description => "Сетевой магазин. 24 часа." ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.315657",  :lat => "54.985305", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.255034",  :lat => "55.031359", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.312942",  :lat => "54.982326", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.303356",  :lat => "54.990044", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.411408",  :lat => "54.9728", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.354133",  :lat => "54.998634", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.313172",  :lat => "54.942345", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.416614",  :lat => "54.990864", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.400404",  :lat => "54.990624", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.295948",  :lat => "54.999107", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.305626",  :lat => "55.046514", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.270701",  :lat => "55.035708", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.378323",  :lat => "55.030199", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.262892",  :lat => "55.041116", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.389697",  :lat => "54.94084", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.440196",  :lat => "54.942831", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.444189",  :lat => "54.961663", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.218931",  :lat => "55.03332", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.455394",  :lat => "54.915539", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.465871",  :lat => "54.92583", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
+    Point.new(:user => User.find_by_id(1),:lng =>"73.508336",  :lat => "54.934125", :point_type => "shop", :name => "Снежана", :description => "Большой выбор. " ).save
   end
   def down
-    Point.delete_all("name = 'Алкоголь 24 часа, сеть продовольственных магазинов'")
+    Point.delete_all("name = 'Снежана'")
   end
 end
