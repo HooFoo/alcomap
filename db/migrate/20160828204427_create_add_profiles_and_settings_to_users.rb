36317cb3d50fd308e4ba559d42669bf653abe086
Gennadiy Khudyakov <lHooFool@gmail.com>
2016-05-12 18:03:52 +0300

    Settings fix

--- db/migrate/20160828204427_create_add_profiles_and_settings_to_users.rb
+++ db/migrate/20160828204427_create_add_profiles_and_settings_to_users.rb
@@ -1,12 +1,16 @@
 class CreateAddProfilesAndSettingsToUsers < ActiveRecord::Migration
   def up
     User.all.each do |user|
-      user.setting ||= Setting.new(:json => '{"shops":true,"bars":true,"markers":true,"messages":true,"users":true}',
-                                   :user_id => user.id)
-      user.profile ||= Profile.new(:age => 18,
-                                   :sex => 'male',
-                                   :comment => '',
-                                   :user_id => user.id)
+      if user.setting.nil?
+        user.setting = Setting.new(:json => '{"shops":true,"bars":true,"markers":true,"messages":true,"users":true}',
+                                     :user_id => user.id)
+      end
+      if user.profile.nil?
+        user.profile = Profile.new(:age => 18,
+                                     :sex => 'male',
+                                     :comment => '',
+                                     :user_id => user.id)
+      end
       user.save!
     end
   end
