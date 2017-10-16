a3d9193e51b6ce3cffce1756bda6f5b21aa1f386
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-10-16 03:30:40 +0300

    Oauth ignore state options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET']
+  provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'], {:provider_ignores_state => true}
 end
