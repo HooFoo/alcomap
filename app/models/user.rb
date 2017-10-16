e45395906058ff5dc8124b4663cc1d5a70190b29
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-10-16 04:50:04 +0300

    Fixed oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,6 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.skip_confirmation!
     end
   end
 
