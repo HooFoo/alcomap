2b5bf08f18c073c9f017579ecee7a0991207e22d
Gennadiy Khudyakov <lHooFool@gmail.com>
2017-10-16 04:10:13 +0300

    Oauth code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
+    Rails.logger.info(request)
+    Rails.logger.info(request.params)
+    Rails.logger.info(request.env["omniauth.auth"])
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
