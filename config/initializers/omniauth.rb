035065e78d587443bad4b3a1aa14ffb9cc4e4f09
Gennadiy Khudyakov <lHooFool@gmail.com>
2019-08-01 09:39:53 +0200

    Info fields

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'], {:provider_ignores_state => true}
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email, first_name, last_name', display: 'popup'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'first_name,last_name', display: 'popup'
 end
